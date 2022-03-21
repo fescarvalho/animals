fe6f084cac153c4766e9bebf71c8999a4d50850b
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2022-03-21 15:31:17 -0300

    fetchBitcoin refatorado

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
+/* SCROLL SUAVE */
 import ScrollSmooth from "./modules/scrollSuave.js";
 const scrollSmooth = new ScrollSmooth('[data-menu="suave"] a[href^="#"]');
 scrollSmooth.init();
 
+/* ACCORDION FAQ */
 import AccordionFaq from "./modules/accordionFaq.js";
 const accordion = new AccordionFaq('[data-menu="accordion"] dt');
 accordion.init();
 
+/* SLIDE DE ANIMAIS */
 import Tabnav from "./modules/navTab.js";
 const tabnav = new Tabnav(
   '[data-tab="menu"] li',
@@ -13,6 +16,7 @@ const tabnav = new Tabnav(
 );
 tabnav.init();
 
+/* MODAL LOGIN */
 import Modal from "./modules/modalLogin.js";
 const modal = new Modal(
   "[data-modal='abrir']",
@@ -21,12 +25,18 @@ const modal = new Modal(
 );
 modal.init();
 
+/* MAPA TOOLTIP */
 import ToolTip from "./modules/toolTip.js";
 const toolTip = new ToolTip("[data-tooltip]");
 toolTip.init();
 
+/* FETCH ANIMAIS */
 import fetchAnimais from "./modules/animais-fetch.js";
 fetchAnimais("../animaisapi.json", ".numeros-grid");
+
+/* FETCH BITCOIN */
+import fetchBitcoin from "./modules/fetchBitcoin.js";
+fetchBitcoin("https://blockchain.info/ticker", ".bitcoin");
 
 import scrollAnimation from "./modules/animacaoScroll.js";
 scrollAnimation();
@@ -35,8 +45,6 @@ initMenuMobile();
 
 import initDataObject from "./modules/dataObject.js";
 initDataObject();
-import initFetchBitcoin from "./modules/fetchBitcoin.js";
-initFetchBitcoin();
 /* import initScrollImg from "./modules/scrollimg.js";
 initScrollImg(); */
 /* import outsideClick from "./module/outsideClick.js";
